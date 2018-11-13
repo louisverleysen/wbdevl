c10db0ef3fac198e24f077360b16a59163982674
louisverleysen <louis.verleysen.student.howest.be>
2018-11-13 14:13:55 +0100

    aanpassingen 13/11

--- Project_Oplossing/index.php
+++ Project_Oplossing/index.php
@@ -6,18 +6,9 @@
     <?php include_once 'views/shared/_nav.inc';?>
 </header>
 <main>
-    <section class="container">
-        <h1>boeken beurs</h1>
-        <p>Hieronder kan je de verschillende tabellen zien die je kan ondervragen via de API.</p>
-        <ul>
-            <li><a href="<?php print($DB)?>/tblboeken">tblboeken</a></li>
-            <li><a href="<?php print($DB)?>/tblboekinformatie">tblboekinformatie</a></li>
-            <li><a href="<?php print($DB)?>/tblklant">tblklant</a></li>
-            <li><a href="<?php print($DB)?>/tblkostprijs">tblkostprijs</a></li>
-            <li><a href="<?php print($DB)?>/tblverkoper">tblverkoper</a></li>
-            <li><a href="<?php print($DB)?>/tblverkoperstype">tblverkoperstype</a></li>
-           
-        </ul>
-    </section>
+
+
+</body>
+</html>
 </main>
 <?php include_once 'views/shared/_footer.inc';?>
